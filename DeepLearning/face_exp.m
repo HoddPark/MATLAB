a8ad933e220ee0990a1f3e28cea99a87006ee3b0
박유진 <hoddor@MacBookAir.local>
2021-03-14 19:20:57 +0900

    delet

--- DeepLearning/face_exp.m
+++ DeepLearning/face_exp.m
@@ -6,13 +6,26 @@ add = split(A,',');
 %%
 i_add = add(:,1);
 
-img = imread(i_add(20000));
-Fdetector = vision.CascadeObjectDetector;
-Face = step(Fdetector,img);
-imgFace = (img(Face(1,2):Face(1,2)+Face(1,4),Face(1,1):Face(1,1)+Face(1,3),:));
+for i = 1:1000
+    
+    img = imread(i_add(i));
+    Fdetector = vision.CascadeObjectDetector;
+    Face = step(Fdetector,img);
+    imgFace = (img(Face(1,2):Face(1,2)+Face(1,4),Face(1,1):Face(1,1)+Face(1,3),:));
 
-re_img = imresize(imgFace,[224 224]);
-figure
-imshow(re_img)
+    re_img = imresize(imgFace,[224 224]);
+    figure
+    imshow(re_img)
+    figure
+    imshow(img)
+    keypressed = getkey;
+    while keypressed ~= 13
+        disp('enter next, q exit')
+        keypressed = getkey;
+    end
+
+    
+end
+
 
 %%
