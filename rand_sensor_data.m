336dd689860f2780606d9722c474b928c9306492
박유진 <hoddor@MacBookAir.local>
2021-03-15 16:03:25 +0900

    sensor data get

--- rand_sensor_data.m
+++ rand_sensor_data.m
@@ -1,25 +1,35 @@
-clc; clear;
+clc; clear; close all;
 
 
 for i = 1:1:10
-    temp(i) = i*10 + randi([1 10],1);
+    temp(i) = (i-1)*10 + randi([0 9],1);
 end
 %temp = 10*rand(10,1) + temp;
 R = getR(temp);
-R_dist = getR_dist(R,1) + rand([10 10]);
-
+temp_dist = getR_dist(temp,0.1) + rand([10 10]);
+R_dist = getR_dist(R,0.1) + rand([10 10]);
 
 figure
 hold on
+scatter(temp_dist, R_dist)
+plot(temp,R)
+
+
+%data save as table
+temp_dist = temp_dist';
+R_dist = R_dist';
 for i=1:1:10
-    scatter(temp(i),R_dist(:,i))
+    R_temp_table(2*i-1,:) = temp_dist(i,:);
+    R_temp_table(2*i,:) = R_dist(i,:);
 end
-plot(temp, R)
+
+
+
 
 function R = getR(temp)
- R = 100*(1 + 0.00392*temp);
+    R = 100*(1 + 0.00392*temp);
 end
 
 function R_dist = getR_dist(R,dis)
-R_dist = dis*randn(10,1) + R;
+    R_dist = dis*randn(10,1) + R;
 end
